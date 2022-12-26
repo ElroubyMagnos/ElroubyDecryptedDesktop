32e44dd2f1448cd67ec0b00babb294f7a8d74cef
Abdullah Elrouby <abedallaelrouby@gmail.com>
2022-12-26 15:31:42 +0200

    User login

---
+++ DecryptedDesktop/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecryptedDesktop
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+    }
+}
